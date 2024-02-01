9bf36a36ccafd8fc46aae258cbfea7053aaf9a3d
Arttu <arttu.arponen@bearingpoint.com>
2024-02-01 17:22:16 +0200

    Final commit

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function showMessage(response) {
   if (response === "No") {
     const noButton = document.getElementById("no-button");
-    const container = document.querySelector(".container");
     const maxWidth = window.innerWidth - noButton.offsetWidth;
     const maxHeight = window.innerHeight - noButton.offsetHeight;
 
@@ -20,24 +19,23 @@ function showMessage(response) {
     noButton.style.top = randomY + "px";
 
     // Update text content and hide name message
-    document.getElementById("question").textContent =
-      "The no button is just for visuals";
-    document.getElementById("name").style.display = "none";
+    document.getElementById("text").textContent = "The no button is just for visuals";
+    document.getElementById("title").style.display = "none";
 
     // Optional: You can also add a timeout to reset the position after a few seconds
   }
 
   if (response === "Yes") {
     // Remove name message and no button
-    document.getElementById("name").remove();
+    document.getElementById("title").remove();
     document.getElementById("no-button").remove();
 
     // Update text content, show message, and change image source to "dance.gif"
-    const yesMessage = document.getElementById("question");
-    yesMessage.textContent = "LESGOOO see you on the 14th babygirl😘😘";
+    const yesMessage = document.getElementById("text");
+    yesMessage.textContent = "YAY! See you on the 14th babygirl😘";
     yesMessage.style.display = "block";
     yesMessage.style.fontStyle = "normal";
-    document.getElementsByClassName("image")[0].src = "images/dance.gif";
+    document.getElementsByClassName("image")[0].src = "images/happycat.gif";
 
     // Remove yes button
     document.getElementById("yesButton").remove();
